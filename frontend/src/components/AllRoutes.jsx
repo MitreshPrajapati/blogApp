38d0b13d059447043050e8734394d51223af90ed
Arun-Jawlia <arunkumar08.mk@gmail.com>
2023-02-05 17:16:58 +0530

    Home Login Signup

--- frontend/src/components/AllRoutes.jsx
+++ frontend/src/components/AllRoutes.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Blogs } from '../Pages/Blogs'
+import { Home } from '../Pages/Home'
+import { Login } from '../Pages/Login'
+import { Signup } from '../Pages/Signup'
 
 export const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/blog' element={<Blogs/>}/>
+    </Routes>
   )
 }
