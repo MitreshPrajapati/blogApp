b6b05865828836bc7003c912ee33678538c85c34
Arun-Jawlia <arunkumar08.mk@gmail.com>
2023-02-05 16:17:20 +0530

    A

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './Redux/store'
-import { Provider } from 'react-redux';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Provider store ={store}>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </BrowserRouter>
 );
-
 
 reportWebVitals();
