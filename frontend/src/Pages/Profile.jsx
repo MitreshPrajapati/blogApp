7707fcfbe86e4f40c23ea4ede7a0b727d7242151
Arun-Jawlia <arunkumar08.mk@gmail.com>
2023-02-12 21:28:24 +0530

    Blog app completed with all functionalities

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -9,26 +9,41 @@ import {
   Stack,
   Button,
   useColorModeValue,
+  Container,
+  SimpleGrid,
+  GridItem,
+  Divider,
 } from "@chakra-ui/react";
 import { URL } from "../api";
 import axios  from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Styles from '../components/pages.module.css'
+import { Navbar } from "../components/Navbar";
 
 
 export const Profile = () => {
 
     let CurrentUser = JSON.parse(localStorage.getItem('CurrentUser'))
+    const [user, setUser] = useState({})
+    const [data, setData] = useState([])
     console.log(CurrentUser._id)
 
-    console.log((`${URL}/user/${CurrentUser._id}`))
 
 
     const getProfile = ()=>
     {
-        axios.get(`${URL}/user/${CurrentUser._id}`)
+        axios.get(`${URL}user/${CurrentUser._id}`,
+        {
+            
+                headers: {
+                  "Content-Type": "application/json",
+                  Authentication: `Bearer ${localStorage.getItem("blogToken")}`
+                }
+        })
         .then(res=>
             {
-                console.log(res)
+                // console.log(res)
+                setUser(res.data)
             })
     }
 
@@ -40,8 +55,37 @@ export const Profile = () => {
 
 
 
+
+
+    const getBlogs = () => {
+         axios
+          .get(`${URL}blog/own/posts`, {
+            headers: {
+              "Content-Type": "application/json",
+              Authentication: `Bearer ${localStorage.getItem("blogToken")}`,
+            },
+          })
+          .then((res) => {
+            // console.log(res.data);
+            setData(res.data);
+          });
+      };
+    
+      useEffect(() => {
+        getBlogs();
+      }, []);
+
+   
+
+      console.log(data)
+
+
   return (
-    <Center py={6}>
+    <>
+    <Navbar/>
+    <Box height={'65px'} scrollBehavior='smooth'>
+   </Box>
+    <Center  py={6}>
       <Box
         maxW={"270px"}
         w={"full"}
@@ -61,6 +105,7 @@ export const Profile = () => {
         <Flex justify={"center"} mt={-12}>
           <Avatar
             size={"xl"}
+            name={user?.user_name}
             src={
               "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
             }
@@ -74,9 +119,9 @@ export const Profile = () => {
         <Box p={6}>
           <Stack spacing={0} align={"center"} mb={5}>
             <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
-              John Doe
+              {user?.user_name}
             </Heading>
-            <Text color={"gray.500"}>Frontend Developer</Text>
+            <Text color={"gray.500"}>{user?.email}</Text>
           </Stack>
 
           <Stack direction={"row"} justify={"center"} spacing={6}>
@@ -110,5 +155,40 @@ export const Profile = () => {
         </Box>
       </Box>
     </Center>
-  );
+
+    <br />
+    <Divider/>
+    <br />
+    <Box>
+        <Text textAlign={'center'} fontSize={'xl'}>My Blogs</Text>
+    </Box>
+    <br/>
+    <Container boxShadow='rgba(0, 0, 0, 0.16) 0px 1px 4px' maxW={['100%','80%','80%']}>
+        <Box>
+            <SimpleGrid p={3} columns={[1,2,3]} spacing={3} gap={5}>
+                {
+                    data.length>0 ? (data.map((blogs)=>
+                    {
+                        return(
+                            <GridItem borderRadius='5px' boxShadow='rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px'>
+                                <Box p={5} margin={'auto'}>
+                                    <Image src ={blogs.images}  borderRadius='5px' />
+                                    <Text fontSize={['md', 'lg']} mt={4} textAlign='center' fontWeight="700">{blogs.title}</Text>
+                                    <Text className={Styles.blog_desc}>{blogs.desc}</Text>
+
+                                </Box>
+                            </GridItem>
+                        )
+
+                    }))
+
+                    :  'else'
+                }
+            </SimpleGrid>
+            <br />
+
+        </Box>
+    </Container>
+    </>
+  )
 };
